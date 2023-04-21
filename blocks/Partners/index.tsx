698b649d24887ece52293d5f652a4f4771eab4b7
Oleh Kulykovskyi <oleh.kvsk@gmail.com>
2023-04-21 12:26:31 +0300

    partial

--- blocks/Partners/index.tsx
+++ blocks/Partners/index.tsx
@@ -1,13 +1,39 @@
-import styles from "@/blocks/Grants/Grants.module.css";
+import additionalStyles from "@/blocks/Grants/Grants.module.css";
+import styles from "./Partners.module.css";
+import science from "assets/images/partners/science.svg";
+import splot from "assets/images/partners/splot.svg";
+import Image, { StaticImageData } from "next/image";
 
+interface Partner {
+  img: StaticImageData;
+  link: string;
+}
+
+const partnersArr: Partner[] = [
+  {
+    img: splot,
+    link: "https://www.ua.splot.institute/",
+  },
+  {
+    img: science,
+    link: "https://www.sciencenow.studio/",
+  },
+];
 const Partners = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.text}>
+    <div className={additionalStyles.container}>
+      <div className={additionalStyles.text}>
         <h2>
           careers
           <br /> and grants
         </h2>
+      </div>
+      <div className={styles.linkWrapper}>
+        {partnersArr.map(({ img, link }, index) => (
+          <a href={link} key={index}>
+            <Image src={img} alt={""} />
+          </a>
+        ))}
       </div>
     </div>
   );
